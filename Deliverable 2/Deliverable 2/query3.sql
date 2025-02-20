7bb195ee31b3d08a1a15def24f0a492832e163cb
Giorgos Pittis <giorgospittis3@gmail.com>
2025-02-21 01:31:21 +0200

    Delete Deliverable 2 directory

--- Deliverable 2/Deliverable 2/query3.sql
+++
@@ -1,5 +0,0 @@
--- Query: USER_COUPONS
-SELECT User.user_name_surname, Coupon.code, Coupon.discount_amount, Coupon.is_active
-FROM User
-JOIN User_has_Coupon ON User.user_id = User_has_Coupon.user_id
-JOIN Coupon ON User_has_Coupon.code = Coupon.code;
